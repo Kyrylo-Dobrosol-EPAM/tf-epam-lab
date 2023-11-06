6aa7fde804f43c2bc8e002a6c2ffd1a50939fbe2
Kyrylo Dobrosol <Kyrylo_Dobrosol@epam.com>
2023-11-06 10:15:48 +0200

    Task 8

--- compute/application.tf
+++ compute/application.tf
@@ -1,28 +1,15 @@
 resource "aws_launch_template" "Kyrylo_Dobrosol_01_launch_template" {
     name = "epam-aws-tf-lab"
     
-    ebs_optimized = true
-    
     iam_instance_profile {
       name = local.base_outputs.iam_instance_profile_name
     }
 
-    update_default_version = true
-    
-    image_id = "ami-05c13eab67c5d8861"
-    
+    update_default_version = true    
+    image_id = "ami-05c13eab67c5d8861"    
     instance_type = "t2.micro"
     
-    placement {
-      availability_zone = "${local.aws_region}a"
-    }
-
-    network_interfaces {
-        associate_public_ip_address = true
-        security_groups = [local.base_outputs.security_group_id_ssh, local.base_outputs.security_group_id_http]
-    }
-    
-    #vpc_security_group_ids = [local.base_outputs.security_group_id_ssh, local.base_outputs.security_group_id_http]
+    vpc_security_group_ids = [local.base_outputs.security_group_id_http_lb]
 
     user_data = "${base64encode(data.template_file.init_script.rendered)}"
 }
@@ -45,15 +32,20 @@ resource "aws_autoscaling_group" "epam-aws-tf-lab-asg" {
       id      = aws_launch_template.Kyrylo_Dobrosol_01_launch_template.id
       version = "$Latest"
     }
+
+    lifecycle {
+      ignore_changes = ["load_balancers", "target_group_arns"]
+    }
 }
 
 resource "aws_lb" "epam-aws-tf-lab-lb" {
     name               = "epam-aws-tf-lab-lb"
     load_balancer_type = "application"
-    security_groups    = [local.base_outputs.security_group_id_http_lb]
+    security_groups    = [local.base_outputs.security_group_id_http_lb,local.base_outputs.security_group_id_ssh,local.base_outputs.security_group_id_http]
     subnets            = local.base_outputs.public_subnet_ids
     
-    enable_deletion_protection = true
+    enable_deletion_protection = false
+    internal = true
 
     tags = {
       Name = "${local.owner}_01_epam-aws-tf-lab-lb"
@@ -63,7 +55,25 @@ resource "aws_lb" "epam-aws-tf-lab-lb" {
     }
 }
 
+resource "aws_lb_target_group" "epam-aws-tf-lab-tg" {
+  name     = "epam-aws-tf-lab-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = local.base_outputs.vpc_id
+}
+
+resource "aws_lb_listener" "epam-aws-tf-lab-l1" {
+  load_balancer_arn = aws_lb.epam-aws-tf-lab-lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.epam-aws-tf-lab-tg.arn
+  }
+}
+
 resource "aws_autoscaling_attachment" "epam-aws-tf-lab-asa" {
     autoscaling_group_name = aws_autoscaling_group.epam-aws-tf-lab-asg.id
-    elb                    = aws_lb.epam-aws-tf-lab-lb.id
+    lb_target_group_arn    = aws_lb_target_group.epam-aws-tf-lab-tg.arn
 }
